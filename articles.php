7343ffe404ede60ae30594ed59867819ff51c7b6
Nichiporchik-Nastya <87989987+Nichiporchik-Nastya@users.noreply.github.com>
2022-11-03 20:48:08 +0300

    Add files via upload

--- articles.php
+++ articles.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Клякса – Главная</title>
+    <title>Клякса – Статьи</title>
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
 
 <?php
+
+
+
 require_once "header.html";
-
-require_once "index-main.html";
-
+require_once "articles-main.html";
 require_once "footer.html";
